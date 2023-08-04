1893e17c7684aa8ea2275799b8b052aad8bfaff4
mevlutkamali <116831651+mevlutkamali@users.noreply.github.com>
2023-08-04 11:59:40 +0300

    Combine_number_and_letter_in R_language
    Combining numbers and letters in "R" language with the help of for loop .

---
+++ number_and_letter_concatenation.R
@@ -0,0 +1,12 @@
+
+number <- c(3,15,22,17,19,2,7)
+
+for(i in number){
+  
+  finish <- paste(i,letters[i])
+  print(finish)
+  
+}
+
+# """ letters """ alfabe kütüphanesi .
+# """ paste """ iki tane elamanı birleştirmeye yarayan fonk.
